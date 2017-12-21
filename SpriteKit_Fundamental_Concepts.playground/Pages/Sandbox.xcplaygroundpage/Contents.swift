51ec4f4e17457199d86828233214051ba9a33e59
Liam Davidson <ldavidson@lcs.on.ca>
2017-12-21 11:56:50 -0500

    Finally starting to understand the actions.

--- SpriteKit_Fundamental_Concepts.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
+++ SpriteKit_Fundamental_Concepts.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
@@ -25,12 +25,23 @@ let nyanCat = SKSpriteNode(imageNamed: "Nyancat")
 nyanCat.position = midPoint
 nyanCat.setScale(8.0)
 
+//adding Luma
+let luma = SKSpriteNode (imageNamed: "LumaSprite")
+luma.position = CGPoint(x: frame.size.width / 2 - 100, y: frame.size.height / 2.0 + 10)
+luma.setScale(0.18)
+scene.addChild(luma)
 //: To show how happy Nyan Cat is, let's run an action that repeats a sequence of Nyan Cat going up and down forever.
-let actionMoveUp = SKAction.moveBy(x: 0, y: 10, duration: 0.15)
-let actionMoveDown = SKAction.moveBy(x: 0, y: -10, duration: 0.15)
-let actionSequence = SKAction.sequence([actionMoveUp, actionMoveDown])
+let actionMoveUp = SKAction.moveBy(x: 0, y: 50, duration: 0.5)
+let actionMoveDown = SKAction.moveBy(x: 0, y: -50, duration: 0.5)
+let actionRotate = SKAction.rotate(byAngle: -15.5, duration: 1.25)
+let actionRotateReverse = SKAction.rotate(byAngle: 15.5, duration: 1.25)
+let actionMoveRight = SKAction.moveBy(x: 50, y: 0, duration: 0.5)
+let actionMoveleft = SKAction.moveBy(x: -50, y: 0, duration: 0.5)
+let actionSequence = SKAction.sequence([actionMoveUp, actionRotate, actionMoveRight, actionRotate, actionMoveDown, actionRotateReverse, actionMoveleft, actionRotateReverse])
 let actionRepeat = SKAction.repeatForever(actionSequence)
 nyanCat.run(actionRepeat)
+luma.run(actionRepeat)
+luma.zPosition = 10
 nyanCat.zPosition = 10  // Ensure sprite is above background
 scene.addChild(nyanCat) // Add to the scene
 
